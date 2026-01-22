e3b6b96a6f0750e88ab77aaef7f977aed42e738a
Noah Jörgensen <silvergrundnoah@gmail.com>
2026-01-22 15:25:05 +0100

    22-01-26

--- Assets/GameTesting/XPManager.cs
+++ Assets/GameTesting/XPManager.cs
@@ -8,10 +8,10 @@ public class XPManager : MonoBehaviour
     AnimationCurve _XPReqScaling;
     [SerializeField]
     UnityEvent<int, int> _onXPGained,
-        _onLevelGained;
+        _onLevelsGained;
 
     public event Action<int, int> OnXPGainedEvent,
-        OnLevelGainedEvent;
+        OnLevelsGainedEvent;
 
     int _currentXP,
         _currentLevel,
@@ -20,13 +20,29 @@ public class XPManager : MonoBehaviour
     public int CurrentXP
     {
         get => _currentXP;
-        set => AddXP(Mathf.Max(0, value));
+        set => AddXP(Mathf.Max(0, _currentXP - value));
     }
 
     public int CurrentLevel => _currentLevel;
 
-    public void AddXP(int value)
+    public void AddXP(int toAdd)
     {
-
+        int prevLevel = _currentLevel;
+        int prevXP = _currentXP;
+        
+        _currentXP += toAdd;
+        
+        _onXPGained?.Invoke(prevXP, _currentLevel);
+        OnXPGainedEvent?.Invoke(prevXP, _currentLevel);
+        
+        while (_currentXP >= _currentXPReq)
+        {
+            _currentLevel++;
+            _currentXP -= _currentXPReq;
+            _currentXPReq = Mathf.CeilToInt(_currentLevel);
+        }
+        
+        _onLevelsGained.Invoke(prevLevel, _currentLevel);
+        OnLevelsGainedEvent?.Invoke(prevLevel, _currentLevel);
     }
 }
